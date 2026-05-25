4a3cbab3c069f7757183e72930492097bb0e10b8
Sidddotdev <siddpvt123@gmail.com>
2026-05-25 23:26:33 +0530

    added routes

---
+++ server/routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/upload-file", (req, res) => {
+ 
+});
+
+router.get("/download-file", (req, res) => {
+ 
+});
+
+router.get("/Delete-file", (req, res) => {
+ 
+});
+
+module.exports = router;
